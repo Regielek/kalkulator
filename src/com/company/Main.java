78b357f0deb1d135eb7de133b05674ceaf05db54
unknown <ferlmick857@gmail.com>
2020-07-03 12:55:30 +0200

    poprawka

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,57 +4,45 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void main(String[] args)
+    public static double getValue(String v)
     {
-        double x, y, z;
+        double x, y, addition;
+        //subtraction, multilication, division;
         x = y = 0;
 
-        boolean a, b;
-        a = b = true;
+        boolean a;
+        a = true;
 
         Scanner X = new Scanner(System.in);
 
-        System.out.print("Liczba x: ");
-
         do
+        {
+            try
             {
-        try
-        {
-            x = Double.parseDouble
-                    (
-                            X.nextLine().replace(",", ".")
-                    );
-            a = false;
-        } catch (NumberFormatException e)
-        {
-            System.out.println("To nie jest liczba! ");
-            a = true;
-            System.out.print("Liczba x: ");
-        }
-            }while (a);
-
-        System.out.print("Liczba y: ");
-
-        do
+                System.out.print("Podaj liczbę " + v + ": ");
+                x = Double.parseDouble
+                        (
+                                X.nextLine().replace(",", ".")
+                        );
+                a = false;
+            } catch (NumberFormatException e)
             {
-        try
-        {
-            y = Double.parseDouble
-                    (
-                            X.nextLine().replace(",", ".")
-                    );
-            b = false;
-        } catch (NumberFormatException e)
-        {
-            System.out.println("To nie jest liczba! ");
-            b = true;
-            System.out.print("Liczba y: ");
-        }
-            }while(b);
+                System.out.println("To nie jest liczba! ");
+                a = true;
+            }
+        }while (a);
 
-        z = x + y;
+        return x;
+    }
 
-        System.out.println(z);
+    public static void main(String[] args)
+    {
+       double x, y;
+
+       x = getValue("x");
+       y = getValue("y");
+
+       double z = x + y;
+       System.out.println("Suma: " +z);
     }
 }
-
