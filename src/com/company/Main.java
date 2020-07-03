aa3c2547eba1dc19f944d25328468429ad0750f2
unknown <ferlmick857@gmail.com>
2020-07-03 11:21:56 +0200

    poprawka

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,20 +6,53 @@ public class Main
 {
     public static void main(String[] args)
     {
+        double x, y, z;
+        x = y = 0;
+
+        boolean a, b;
+        a = b = true;
+
         Scanner X = new Scanner(System.in);
 
         System.out.print("Liczba x: ");
-        double x = Double.parseDouble
-                (
-                        X.nextLine().replace(",", ".")
-                );
-        System.out.print("Liczba y: ");
-        double y = Double.parseDouble
-                (
-                        X.nextLine().replace(",", ".")
-                );
 
-        double z = x + y;
+        do
+            {
+        try
+        {
+            x = Double.parseDouble
+                    (
+                            X.nextLine().replace(",", ".")
+                    );
+            a = false;
+        } catch (NumberFormatException e)
+        {
+            System.out.println("To nie jest liczba! ");
+            a = true;
+            System.out.print("Liczba x: ");
+        }
+            }while (a);
+
+        System.out.print("Liczba y: ");
+
+        do
+            {
+        try
+        {
+            y = Double.parseDouble
+                    (
+                            X.nextLine().replace(",", ".")
+                    );
+            b = false;
+        } catch (NumberFormatException e)
+        {
+            System.out.println("To nie jest liczba! ");
+            b = true;
+            System.out.print("Liczba y: ");
+        }
+            }while(b);
+
+        z = x + y;
 
         System.out.println(z);
     }
