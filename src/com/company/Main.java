d7c34d145b020eaf5afc73164f0a8154b473d1a3
unknown <ferlmick857@gmail.com>
2020-07-03 10:25:43 +0200

    poprawka

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,17 +7,21 @@ public class Main
     public static void main(String[] args)
     {
         Scanner X = new Scanner(System.in);
-        Scanner Y = new Scanner(System.in);
 
         System.out.print("Liczba x: ");
-        String tmp = X.nextLine().replace(",", ".");
-        double x = Double.parseDouble(tmp);
+        double x = Double.parseDouble
+                (
+                        X.nextLine().replace(",", ".")
+                );
         System.out.print("Liczba y: ");
-        String tmp2 = X.nextLine().replace(",", ".");
-        double y = Double.parseDouble(tmp2);
+        double y = Double.parseDouble
+                (
+                        X.nextLine().replace(",", ".")
+                );
 
         double z = x + y;
 
         System.out.println(z);
     }
 }
+
